975b155306d3adb79a8427c7e777df16c234129c
saveTochi <nwangumat@hotmail.com>
2019-11-15 12:43:31 +0100

    Change currency from usd to naira

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,7 @@ import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  const formattedExpensesTotal = `₦${numeral(expensesTotal / 100).format('0,0.00')}`;
 
   return (
     <div>

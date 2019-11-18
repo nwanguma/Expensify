b194564faad91527a3587b83a80d4bea67d331d1
saveTochi <nwangumat@hotmail.com>
2019-11-18 01:14:34 +0100

    Style nav position

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({ startLogout }) => (
     <h1 className="header__heading">Expensify</h1>
     <nav className="header__nav">
       <NavLink to="/dashboard" activeClassName="is-active" className="header__nav__item header__nav__item--dashboard">Dashboard</NavLink>
-      <NavLink to="/create" activeClassName="is-active" className="header__nav__item header__nav__item--create">Create Expense</NavLink>
+      <NavLink to="/create" activeClassName="is-active" className="header__nav__item header__nav__item--create">New Expense</NavLink>
       <button onClick={startLogout} className="btn btn--logout">Log out</button>
     </nav>
   </header>
